68d70a298c633bf14f3d6b58f4d61efe170468ee
Jan Böcker <jan.boecker@jboecker.de>
2014-07-08 10:42:45 +0200

    ran bower install

---
+++ src/bower_components/proj4/proj4/projCode/longlat.js
@@ -0,0 +1,11 @@
+define(function(require, exports) {
+  exports.init = function() {
+    //no-op for longlat
+  };
+  function identity(pt){
+    return pt;
+  }
+  exports.forward = identity;
+  exports.inverse = identity;
+  exports.names = ["longlat", "identity"];
+});
